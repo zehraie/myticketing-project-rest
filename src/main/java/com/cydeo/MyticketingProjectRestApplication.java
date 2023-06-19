34270395107118ef797d458867ffdf100a27e741
zehrairkicatal <zehraie197@gmail.com>
2023-06-19 23:36:04 +0100

    creating ResponseWrapper

--- src/main/java/com/cydeo/MyticketingProjectRestApplication.java
+++ src/main/java/com/cydeo/MyticketingProjectRestApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyticketingProjectRestApplication {
@@ -9,5 +11,11 @@ public class MyticketingProjectRestApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyticketingProjectRestApplication.class, args);
     }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
 
 }
